b491ce6520dbedf8000e7ff67901f9264c1ee129
Mark Allen <mark@markcallen.com>
2015-04-27 22:05:34 -0700

    Added startlinefix and time to kill

--- nmeasocketio.js
+++ nmeasocketio.js
@@ -6,6 +6,7 @@ var querystring = require('querystring');
 var LineByLineReader = require('line-by-line');
 var utils = require("./utils.js");
 var seriallistener = require('./seriallistener');
+var ivector = require('./intersect');
 
 var http = require('http');
 
@@ -22,6 +23,8 @@ function parsePost(req, callback) {
     callback(data);
   });
 }
+
+var filename = null;
 
 var server = http.createServer(function(request, response){
         var path = url.parse(request.url).pathname;
@@ -49,6 +52,7 @@ var server = http.createServer(function(request, response){
                 });
                 break;
             case '/demomode/start': 
+	      filename = "nmeademo.txt";
               writeFileToSocketIO();
               response.writeHead(200, {"Content-Type": "text/html"});
               response.write("demomode started");
@@ -56,8 +60,31 @@ var server = http.createServer(function(request, response){
               break;
             case '/demomode/stop': 
               runningDemoMode = false;
+              try {
+                clearTimeout(writeTimeout);
+              }
+              catch(err) {
+	      }
               response.writeHead(200, {"Content-Type": "text/html"});
               response.write("demomode stopped");
+              response.end();
+              break;
+            case '/demostartmode/start': 
+	      filename = "nmeastartdemo.txt";
+              writeFileToSocketIO();
+              response.writeHead(200, {"Content-Type": "text/html"});
+              response.write("demostartmode started");
+              response.end();
+              break;
+            case '/demostartmode/stop': 
+              runningDemoMode = false;
+              try {
+                clearTimeout(writeTimeout);
+              }
+              catch(err) {
+	      }
+              response.writeHead(200, {"Content-Type": "text/html"});
+              response.write("demostartmode stopped");
               response.end();
               break;
             case (path.match(/^\/startcountdown/) || {}).input:
@@ -66,9 +93,9 @@ var server = http.createServer(function(request, response){
                 if (countdown == parseInt(countdown, 10)) {
                   response.writeHead(200, {'Content-Type': 'text/html'});
                   response.write(JSON.stringify(countdown));
-                  if (currentTimeout != null) {
+                  if (countdownTimeout != null) {
                     try {
-                      clearTimeout(currentTimeout);
+                      clearTimeout(countdownTimeout);
                     }
                     catch(err) {
                     }
@@ -81,14 +108,39 @@ var server = http.createServer(function(request, response){
                 }
                 response.end();
                 break;
+	    case '/stopcountdown': 
+                if (countdownTimeout != null) {
+                  try {
+                    clearTimeout(countdownTimeout);
+                  }
+                  catch(err) {
+                  }
+                }
+		break;
             case (path.match(/^\/startlinefix/) || {}).input:
                 var parts = path.split("/");
                 response.writeHead(200, {'Content-Type': 'text/html'});
                 var startlinefix = {latitude: parts[2], hemi: parts[3], longitude: parts[4], easting: parts[5], bearing: parts[6] };
                 response.write(JSON.stringify(startlinefix));
                 response.end();
-                writeMsg(startlinefix);
+                if (startlinefixTimeout != null) {
+                  try {
+                    clearTimeout(startlinefixTimeout);
+                  }
+                  catch(err) {
+                  }
+                }
+		runStartLineFix(startlinefix);
                 break;
+	    case '/stoplinefix': 
+                if (startlinefixTimeout != null) {
+                  try {
+                    clearTimeout(startlinefixTimeout);
+                  }
+                  catch(err) {
+                  }
+                }
+		break;
             case '/admin/parsers/show': 
 		try {
 		  var parsers = JSON.parse(fs.readFileSync(__dirname + '/parsers.json', 'utf8'));
@@ -133,20 +185,19 @@ server.listen(webserver.port);
 var serialPort = require("serialport");
 
 var io = require('socket.io')(server);
+
 setupSocketIO(function() {
     console.log("Ready");
 });
 
 function setupSocketIO(cb) {
-//  io.configure( function() {
-//    io.set('log level', 1)
-//    io.set('close timeout', 60*60); // 1h time out
-//    io.set('heartbeat timeout', 60*60); // 1h time out
-//  });
-
-  seriallistener.getSerialPorts(writeData);
+//  seriallistener.getSerialPorts(writeData);
   cb && cb();
 }
+
+var currentlat = null;
+var currentlon = null;
+var currentspeed = null;
 
 function writeData(port, sentence) {
   try {
@@ -156,6 +207,21 @@ function writeData(port, sentence) {
     var object = nmea.parse(sentence)
     object.timestamp = Date.now();
     io.emit('nmea', JSON.stringify([object], null, 2));
+    if (object.BLA) {
+      currentlat = object.BLA.value;
+      if (object.BLA.units == "S") {
+        currentlat = -1 * currentlat;
+      }
+    }
+    if (object.BLO) {
+      currentlon = object.BLO.value;
+      if (object.BLO.units == "W") {
+        currentlon = -1 * currentlon;
+      }
+    }
+    if (object.BSP) {
+      currentspeed = object.BSP.value;
+    }
   } catch (exception) {
     var err = {port: port, timestamp: Date.now(), sentence: sentence, exception: exception.message};
     io.emit('err', JSON.stringify([err], null, 2));
@@ -163,83 +229,12 @@ function writeData(port, sentence) {
   }
 }
 
-
-
-function portLogger(port) {
-
-  var SerialPort = require("serialport").SerialPort
-  var serialport = new SerialPort(port, {
-    baudrate: 4800,
-    parser: serialPort.parsers.readline("\n")
-  }, false); // this is the openImmediately flag [default is true]
-
-  serialport.open(function (error) {
-    if ( error ) {
-      console.log('failed to open: '+error);
-    } else {
-      var sentence = "";
-      serialport.on('data', function(data) {
-          checksum = data.split('*');
-          if(checksum.length === 2) {
-              // there is a checksum
-              sentence = checksum[0];
-              checksum = checksum[1];
-          } else {
-              checksum = null;
-          }
-          if (checksum && utils.verifyChecksum(sentence, checksum)) {
-            //console.log(data);
-            try {
-              var object = nmea.parse(sentence)
-              var timestamp = Date.now();
-              if ("angle" in object && "speed" in object) {
-                var obj1 = {name: "AWA", value: object.angle, units: object.reference, timestamp: Date.now()};
-                var obj2 = {name: "BSP", value: object.speed, units: object.units, timestamp: Date.now()};
-  
-                obj1.timestamp = timestamp;
-                obj2.timestamp = timestamp;
-                io.emit('nmea', JSON.stringify([obj1, obj2], null, 2));
-              } else if ("latitude" in object && "longitude" in object) {
-                // throw this away if no satellites were used.
-                if (object.satellites) {
-                  var obj1 = {name: "LAT", value: object.latitude, timestamp: Date.now()};
-                  var obj2 = {name: "LON", value: object.longitude, timestamp: Date.now()};
-  
-                  obj1.timestamp = timestamp;
-                  obj2.timestamp = timestamp;
-                  io.emit('nmea', JSON.stringify([obj1, obj2], null, 2));
-                }
-              } else if ("course" in object && "knots" in object) {
-                  var obj1 = {name: "SPD", value: object.knots, timestamp: Date.now()};
-                  var obj2 = {name: "CRS", value: object.course, timestamp: Date.now()};
-  
-                  obj1.timestamp = timestamp;
-                  obj2.timestamp = timestamp;
-                  io.emit('nmea', JSON.stringify([obj1, obj2], null, 2));
-              } else {
-                //console.log(object);
-              }
-            } catch (exception) {
-              var error = {sentense: sentense, message: exception};
-              console.log(JSON.stringify(error));
-            }
-          }
-      });
-    }
-  });
-
-}
-
-function writeMsg(message) {
-  io.emit('msg', JSON.stringify(message, null, 2));
-}
-
-var currentTimeout = null;
+var countdownTimeout = null;
 
 function runCountDown(countdown) {
   if (countdown >= 0) {
     io.emit('countdown', countdown); 
-    currentTimeout = setTimeout(
+    countdownTimeout = setTimeout(
       function() { 
         runCountDown(--countdown);
       }, 
@@ -247,10 +242,33 @@ function runCountDown(countdown) {
   }
 }
 
-var filename = 'nmeademo.txt';
+var startlinefixTimeout = null;
+
+function runStartLineFix(startlinefix) {
+  if (startlinefix && currentlat && currentlon && currentspeed) {
+    var startpoint = {x: startlinefix.latitude, y: startlinefix.longitude};
+    if (startlinefix.hemi == "S") {
+      startpoint.x = -1 * startpoint.x;
+    }
+    if (startlinefix.easting == "W") {
+      startpoint.y = -1 * startpoint.y;
+    }
+    var startline = ivector.getLine(startpoint, startlinefix.bearing, 1/3600);
+    var distance = ivector.getDistance({x: currentlat, y: currentlon}, startline);
+    var timetokill = currentspeed / distance;
+    io.emit('ttk', [timetokill]); 
+    startlinefixTimeout = setTimeout(
+      function() { 
+        runStartLineFix(startlinefix);
+      }, 
+    1000);
+  }
+}
+
+var writeTimeout = null;
 
 function writeFileToSocketIO() {
-  if (! runningDemoMode) {
+  if (! runningDemoMode && filename) {
     runningDemoMode = true;
     var stream = new LineByLineReader(filename);
     stream.on('end', function() {
@@ -269,7 +287,7 @@ function writeFileToSocketIO() {
       if (runningDemoMode) {
         writeData('demo', line);
       }
-      var timer = setTimeout(function() {
+      writeTimeout = setTimeout(function() {
           stream.resume();
       }, 1000);
     });
