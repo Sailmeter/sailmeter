69f56717d2e084f567214faeb2566277acef18f6
Mark Allen <mark@markcallen.com>
2015-05-30 11:19:27 -0700

    Created update script to pull master from git

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e 
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+stop nmeasocketio
+
+su ubuntu -c 'git stash && git pull && git stash pop'
+
+su ubuntu -c 'npm install --production'
+
+echo "Now running " `cat version.txt`
+
+start nmeasocketio
+
+HOSTNAME=`hostname`
+
+echo Connect to $HOSTNAME
+echo Go to http://172.16.10.1:8888/admin.html
+
